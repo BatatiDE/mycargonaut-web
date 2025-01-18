540c9b7333dd763664e5ff408479f1f038980e91
Patrick <patrick.solka@mni.thm.de>
2025-01-18 20:18:10 +0100

    Update searchProps.ts

---
+++ src/types/searchProps.ts
@@ -0,0 +1,15 @@
+import {Trip} from "@/types/trip";
+
+export interface SearchProps {
+    action: (criteria: SearchCriteria) => void;
+}
+
+export interface SearchCriteria {
+    startingPoint: string;
+    destinationPoint: string;
+    date: Date | undefined;
+    passengerSeats: number;
+    freightSpace: number;
+    isFreightOnly: boolean;
+    filteredRides: Trip[];
+}
